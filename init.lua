198d7d3afa6f171909bc450e728695721ff1ea95
Sebastian Drela <sebastian.drela@wp.pl>
2025-07-03 02:06:20 +0200

    disable raygun cursor

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+vim.g.neovide_cursor_animation_length = 0
+vim.g.neovide_cursor_trail_length = 0
+vim.g.neovide_cursor_vfx_mode = ""
+
 require('plugins.init')
 require('core.options')
 require('core.keymaps')
